f81862766fd82cbf29c61950f17bc64a11d17cda
Aidan Matzko <amatzko48@gmail.com>
2012-06-21 03:56:26 -0400

    Update home with the initial table and a fake entry

--- upload/home.php
+++ upload/home.php
@@ -18,6 +18,27 @@ include('includes/config.php');
     <div id="main_content">
     	<!-- Content! -->
     	<p>Stuff and things like that</p>
+    	<table id="Banned">
+    		<!-- 
+    		Banned table
+    		Colums:
+    		Name - Moderator - Time - Reason - Approved
+    		-->
+            <tr>
+            	<th>Name</th>
+            	<th>Moderator</th>
+            	<th>Time</th>
+            	<th>Reason</th>
+            	<th>Approved</th>
+            </tr>
+            <tr>
+                <td>User1234</td>
+                <td>SuperModerator1</td>
+                <td>12:32AM</td>
+                <td>Being a general idiot</td>
+                <td>False</td>
+            </tr>
+    	</table>
     </div>
     <footer id="credits">
 		<!-- Credits -->
