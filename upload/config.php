af1752527bc9678df5a8787559f0d45ee45135b2
AgentKid <agentkid20@gmail.com>
2012-06-21 02:32:11 -0400

    Reorganized, Added siteName config.

--- upload/config.php
+++ upload/config.php
@@ -22,18 +22,24 @@ $config['mysql']['appealtable'] = 'sqlb_appeals';
 
 
 
-// -------------- General Config Flags --------------
+// -------------- Site Wide Settings --------------
 
 // What is the root domain of the site? Make sure to keep the slashes.
 $config['rootdomain'] = 'http://mysite.com/';
+// Logo to use for all rendered pages.
+$config['logoURL'] = 'http://araeosia.com/bans/logo.png';
+// The name of the site. If you put "Site Name Here", pages would be rendered as "Site Name Here - Home" and "Site Name Here - Ban Appeal"
+$config['siteName'] = 'SQLBans'
+
+
+// -------------- General Config Flags --------------
+
 // Should debug mode be on? This should always be off unless you have a specific reason for it to be on.
 $config['debug'] = true;
 // Have you read the instructions?
 $config['inconspicuousValue'] = false;
 // Warn the moderators about global bans using the FishBans global ban aggregator? http://fishbans.com for info on FishBans.
 $config['useFishBans'] = true;
-// Logo to use for all rendered pages.
-$config['logoURL'] = 'http://araeosia.com/bans/logo.png';
 // Use an array in this file (see Users and passwords) for storing users, or use a MySQL database? If set to false, the SQLUserDB settings must be set
 $config['useSimpleAuth'] = true;
 // These values only apply if useSQLUsers is set to true above. Explanations for each of these values can be found in the MySQL section.
