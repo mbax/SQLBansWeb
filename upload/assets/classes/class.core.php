02126cf8358add7317226ac70a47e003b43922aa
AgentKid <agentkid20@gmail.com>
2012-06-27 12:50:09 -0400

    Fix mbax being a derp. <3

--- upload/assets/classes/class.core.php
+++ upload/assets/classes/class.core.php
@@ -3,10 +3,10 @@
  * Core PHP Functions File
  */
 
-require_once('../../config.php');
-require_once('class.database.php');
-require_once('class.timemanagement.php');
-require_once('class.auth.php');
+require_once('config.php');
+require_once('assets/classes/class.database.php');
+require_once('assets/classes/class.timemanagement.php');
+require_once('assets/classes/class.auth.php');
 
 $sqlbans->database = new database($config['mysql']['user'], $config['mysql']['password'], $config['mysql']['host'], $config['mysql']['port'], $config['mysql']['database']);
 $sqlbans->time = new time();
